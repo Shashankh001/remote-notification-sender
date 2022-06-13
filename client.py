8cf5aae3395f3c0fe6ef828fa7469c43426f6d04
Shashankh Gedda <72354934+Shashankh001@users.noreply.github.com>
2022-06-13 22:15:46 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,11 +1,11 @@
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)#defining a socket
 
-IP = '192.168.0.102'
-PORT = 1234
+IP = '192.168.0.102'#ip (to be changed accordingly)
+PORT = 1234#must be the same on both the ends
 
-notification = input("Enter your message: ")
+notification = input("Enter your message: ")#input for notification
 print("")
 
 
@@ -21,5 +21,5 @@ except:
     print("Hmm, looks like the receiver is offline. Try again later.")
     quit()
 
-s.send(bytes(notification,'utf-8'))
+s.send(bytes(notification,'utf-8')) #send the notification details
 print("Sent notification successfully!")
