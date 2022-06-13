fcb2644e0589711b96b7cc6484a7d1e48bc35154
Shashankh Gedda <72354934+Shashankh001@users.noreply.github.com>
2022-06-13 22:17:52 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,15 +1,16 @@
 import socket
 from win10toast import ToastNotifier
 
-toaster = ToastNotifier()
+toaster = ToastNotifier() #defining the notification toaster
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('0.0.0.0',1234))
-s.listen(5)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)#defining socket
+s.bind(('0.0.0.0',1234))#binding ip
+s.listen(5)#can accept upto 5 clients
 
 while True:
     cs, address = s.accept()
-    notification = cs.recv(1024)
-    notification = notification.decode('utf-8')
+    
+    notification = cs.recv(1024)#recieve the notification message
+    notification = notification.decode('utf-8')#decode the message
 
-    toaster.show_toast(f"New Message from {address}",notification)
+    toaster.show_toast(f"New Message from {address}",notification)#show the notification
